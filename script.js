ea40935e2df9232711a5288beb729c0e052a8d03
The Hustle <saikiran.gujju@proton.me>
2026-08-01 02:21:13 +0530

    local storage part added

--- script.js
+++ script.js
@@ -4,17 +4,28 @@ const taskContent = document.getElementById("task-input")
 const addTaskBtn = document.getElementById("add-btn")
 
 
-let tasks = []
+let tasks = json.parse(localStorage.getItem("tasks")) || []
 addTaskBtn.addEventListener("click" , () => {
     let text = taskContent.value.trim()
-    tasks.push(text)
+    let task = {
+        id : Date.now(),
+        name : text,
+        completed : true
+    }
+    tasks.push(task)
     console.log(tasks)
     text = ""
+    storeItem()
+
 
 })
 
+function
 
 
+function storeItem() {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
 
 
 
